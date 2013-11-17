de79f0b8e12079f228f6acaa6621109d63223e65
Theo Chatzimichos <tampakrap@gentoo.org>
2013-11-17 02:59:24 +0100

    add ability to pass mask in portage::package

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,14 @@ class drupal (
   $ensure          = $drupal::params::ensure,
   $gentoo_use      = $drupal::params::gentoo_use,
   $gentoo_keywords = $drupal::params::gentoo_keywords,
+  $gentoo_mask     = $drupal::params::gentoo_mask,
 ) inherits drupal::params {
 
   portage::package { $pkg_name:
     ensure   => $ensure,
     use      => $gentoo_use,
     keywords => $gentoo_keywords,
+    mask     => $gentoo_mask,
     target   => 'drupal',
   }
 
